39e46916878245daa8a80cfac981d0abf0668aa2
JahnaviPS <69338685+JahnaviPS@users.noreply.github.com>
2021-11-16 20:22:23 +0530

    Add files via upload

---
+++ EmployeeDetailsDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using HRBAL1;
+
+
+namespace HRDAL
+{
+    public class EmployeeDetailsDAL
+    {
+        public DataTable showDetails(int p)
+        {
+            string s = "select * from EmployeeDetails where EmployeeId=" + p;
+            SqlConnection cn = new SqlConnection("Data Source=LAPTOP-JF2QMSQ6\\SQLEXPRESS;Initial Catalog=HRdb;Integrated Security=True");
+            SqlDataAdapter da = new SqlDataAdapter(s, cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "EmployeeDetails");
+              DataTable t= ds.Tables["EmployeeDetails"];
+            return t;
+        }
+    }
+}
