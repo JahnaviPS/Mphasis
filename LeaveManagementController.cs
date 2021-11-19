85a86282e28518daf4e21c38f940c8c9bcae4f21
JahnaviPS <69338685+JahnaviPS@users.noreply.github.com>
2021-11-19 08:52:25 +0530

    Add files via upload

---
+++ LeaveManagementController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using HRBAL1;
+using HRDAL;
+using System.Web.Http.Cors;
+
+namespace HRServiceProject.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+   // [RoutePrefix("api/VTRouting")]
+    public class LeaveManagementController : ApiController
+    {
+        //[HttpGet]
+        //public DataTable DisplayData(int a)
+        //{
+        //    LeaveManagementDAL d = new LeaveManagementDAL();
+        //    LeaveManagement b = new LeaveManagement();
+        //    b.EmpId = a;
+
+        //    DataTable b1 = d.showAll();
+        //    return b1;
+
+
+        //}
+        [HttpPost]
+       // [Route("insertleave")]
+        public IHttpActionResult InsertData([FromBody] LeaveManagement L)
+        {
+            LeaveManagementDAL d = new LeaveManagementDAL();
+            d.add(L);
+
+
+
+            return Ok(new { status = 200, isSuccess = true, message = "Employee added successfully" });
+
+        }
+        [HttpGet]
+        public List<LeaveManagement> showdetails()
+        {
+            LeaveManagementDAL d = new LeaveManagementDAL();
+            List<LeaveManagement> f = d.showdetails();
+            return f;
+
+
+        }
+    }
+}
---
+++ PayRollController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using HRBAL1;
+using HRDAL;
+using System.Web.Http.Cors;
+
+namespace HRServiceProject.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PayRollController : ApiController
+    {
+       
+
+
+        [HttpPost]
+
+        public IHttpActionResult InsertDetais([FromBody] Payroll L)
+        {
+            PayrollDAL d = new PayrollDAL();
+            d.add(L);
+
+
+
+            return Ok(new { status = 200, isSuccess = true, message = "Employee added successfully" });
+
+        }
+        [HttpGet]
+        public List<Payroll> displaypayroll()
+        {
+            PayrollDAL d = new PayrollDAL();
+            List<Payroll> f = d.displaypayroll();
+            return f;
+
+
+        }
+    }
+}
